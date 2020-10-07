166b6bcd0eb8d62d4015640739fcf661f6a1dbc8
lmotta <motta.luiz@gmail.com>
2020-10-07 09:01:57 -0300

    Added VRT files

--- download_inpe_cbers4a_wpm_alg.py
+++ download_inpe_cbers4a_wpm_alg.py
@@ -10,11 +10,12 @@ email                : motta.luiz@gmail.com
 
 Dependences:
 - gdal
-- mod_py
 
 Updates:
 - 2020-10-05:
 Added Canceled
+- 2020-10-07:
+Create VRT files
 
  ***************************************************************************/
 
@@ -44,9 +45,8 @@ from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 gdal.UseExceptions()
 
-from qgis import processing
 from qgis.processing import alg
-
+from qgis.core import QgsProcessingParameterFile
 
 class FilePathType(object):
     def __init__(self):
@@ -66,6 +66,7 @@ class DownloadCbersWpm():
         self.feedback = feedback
         self.c_url, self.t_url, self.f_perc_url = 0, 0, 0
         self.isOkDownload = True
+        self.total_stacks = 0
        
     def readUrls(self):
         with open( self.pathfile, 'r') as content_file:
@@ -120,8 +121,7 @@ class DownloadCbersWpm():
                 self.feedback.setProgress( percent )
                 self.feedback.setProgressText( msg )
 
-            ds = None
-            name = f"{url.split('?')[0].split('/')[-1]}"
+            name = url.split('?')[0].split('/')[-1]
             image = f"{self.pathdir}{os.path.sep}{name}"
             image_download = f"{image}.download"
             if not os.path.exists( image ):
@@ -132,10 +132,11 @@ class DownloadCbersWpm():
                     self.isOkDownload = False
                     if os.path.isfile( image_download ):
                         os.remove( image_download )
-                    return
+                    return name
 
                 shutil.move( image_download, image )
                 if image.split('.')[-1] == 'tif':
+                    ds = None
                     try:
                         ds = gdal.Open( image, GA_ReadOnly )
                     except RuntimeError: # gdal
@@ -143,18 +144,58 @@ class DownloadCbersWpm():
                         msg = f"Url for '{name}': Error open image"
                         self.feedback.reportError( msg )
                         self.isOkDownload = False
-                        return
+                        return name
                     ds = None
                     
             progress( name )
+            return name
+
+        def getImagesStack(names):
+            filterBand = lambda name: name[-4:] == '.tif' and name[-5:] != '0.tif'
+            names = [ name for name in names if filterBand( name ) ] # 1 .. N bands of all images
+            # All suffixImage
+            d_suffix = {}
+            for name in names:
+                suffix = name.split( os.path.sep )[-1][:-5]
+                pathfile = f"{self.pathdir}{os.path.sep}{name}"
+                band = name[-5:-4]
+                if not suffix in d_suffix:
+                    d_suffix[ suffix ] = {
+                        'pathfiles': [ pathfile ],
+                        'bands': [ band ]
+                    }
+                else:
+                    d_suffix[ suffix ]['pathfiles'].append( pathfile )
+                    d_suffix[ suffix ]['bands'].append( band )
+            #
+            stacks = {}
+            for suffix, images_bands in d_suffix.items():
+                bands = ''.join( images_bands['bands'] )
+                stack = f"{self.pathdir}{os.path.sep}{suffix}{bands}.vrt"
+                stacks[ stack ] = images_bands['pathfiles']
+            d_suffix.clear()
+            
+            return stacks
 
         self.t_url = len( urls )
         self.f_perc_url = 100.0 / self.t_url
         
         pool = ThreadPool( processes=4 )
         mapResult = pool.map_async( download, urls )
-        [ r for r in mapResult.get() ]
+        names = [ name for name in mapResult.get() ]
         pool.close()
+
+        if not self.isOkDownload:
+            return
+
+        # Create VRT's
+        stacks = getImagesStack( names )
+        self.total_stacks = len( stacks )
+        for stack, images in stacks.items():
+            if not os.path.isfile( stack ):
+                ds = gdal.BuildVRT( stack, images, separate=True )
+                ds = None
+
 
 @alg(name='cbers4downloadwpmalg', label='Download CBERS WPM files from INPE',
      group='lmottacripts', group_label='Lmotta scripts')
@@ -162,25 +203,39 @@ class DownloadCbersWpm():
 @alg.input(type=alg.FILE, name='FILE_INPE', extension='txt',
            label='Input file Inpe(inpe_catalog_Year_Month_Day_Hour_Minute_Second.txt)')
 
-@alg.input(type=alg.FOLDER_DEST, name='FOLDER_IMAGES', label='Folder of images downloads')
+@alg.input(type=alg.FILE, name='FOLDER_IMAGES', behavior=QgsProcessingParameterFile.Folder,
+           label='Folder of images downloads')
 
-@alg.output(type=alg.STRING, name='MESSAGE', label='Message of processing')
+@alg.output(type=alg.BOOL, name='STATUS', label='Message of processing')
 
 def cbers4downloadwpmalg(instance, parameters, context, feedback, inputs):
     """
-    Download CBERS WPM files from INPE.
+    Download CBERS WPM files from INPE(http://www2.dgi.inpe.br/catalogo/explore).
+    Input:
+      - File with URLs for downloads obtained by INPE.
+        File model 'inpe_catalog_Year_Month_Day_Hour_Minute_Second.txt'.
+      - Directory for download.
+    Output:
+      - CBERS images by band.
+      - VRTs(Vitual Rasters) with stack bands.    
     """
     input_pathfile = instance.parameterAsFile(parameters, 'FILE_INPE', context )
     input_pathdir = instance.parameterAsFile(parameters, 'FOLDER_IMAGES', context )
-
+    input_add_vrt = instance.parameterAsBoolean(parameters, 'ADD_VRT', context )
+    
     dcw = DownloadCbersWpm( input_pathfile, input_pathdir, feedback )
     urls = dcw.readUrls()
     if len( urls ) == 0:
         msg = "Missing files in {pathfile}"
-        self.feedback.reportError( msg )
-        return { 'MESSAGE': msg }
+        feedback.reportError( msg )
+        return { 'STATUS': False }
 
     dcw.downloadsImages( urls )
-    msg = f"Finished all {dcw.t_url} files in '{input_pathdir}'" if dcw.isOkDownload else 'Error download files'
+    if not dcw.isOkDownload:
+        return { 'STATUS': False }
     
-    return { 'MESSAGE': msg }
+    msg = f"\n\nDownload {len( urls )} files in '{input_pathdir}'\nCreated {dcw.total_stacks} Virtual Raster(VRT)\n\n"
+    feedback.pushInfo( msg )
+    
+    return { 'STATUS': True }
+    
